804be0835a720dc21543cc566221bc75a206a06b
Artjom Juferov <artjom.juferov@gmail.com>
2024-12-02 17:31:13 +0200

    Correct paths

--- lib/scripts/pre-push.bash
+++ lib/scripts/pre-push.bash
@@ -5,22 +5,22 @@ echo "heres"
 git fetch
 
 if [ $(git rev-parse HEAD) != $(git rev-parse @{u}) ]; then
-  ./scripts/git/pre-pushs/rubocop.bash
+  ./lib/scripts/pre-pushs/rubocop.bash
   if [ $? != 0 ]; then
     exit 1
   fi
 
-  ./scripts/git/pre-pushs/brakeman.bash
+  ./lib/scripts/pre-pushs/brakeman.bash
   if [ $? != 0 ]; then
     exit 1
   fi
 
-  ./scripts/git/pre-pushs/sorbet.bash
+  ./lib/scripts/pre-pushs/sorbet.bash
   if [ $? != 0 ]; then
     exit 1
   fi
 
-  ./scripts/git/pre-pushs/tests.bash
+  ./lib/scripts/pre-pushs/tests.bash
   if [ $? != 0 ]; then
     exit 1
   fi
