211b398caab6c6ac57e0ac6c8e815bf2315f66f2
Artjom Juferov <artjom.juferov@gmail.com>
2024-12-08 03:21:15 +0200

    Update pre-push, version 0.0.5

--- lib/scripts/pre-push.rb
+++ lib/scripts/pre-push.rb
@@ -19,10 +19,8 @@ def remote_same?
   `git rev-parse HEAD` == `git rev-parse @{u}`
 end
 
-if !remote_exist? || remote_same?
-  `exit 1`
-else
-  CHECKERS.each do |script|
-    return `exit 1` unless system script
-  end
+def any_checker_failed?
+  CHECKERS.any? { |script| !system(script) }
 end
+
+exit 1 if !remote_exist? || remote_same? || any_checker_failed?
