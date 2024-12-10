0cb0b1c9cda902b85ca8bb0abc2bb7ece28551b6
Artjom <AiUferovs@stud.kantiana.ru>
2024-12-10 10:18:08 +0200

    Update sebes_git_prepush.rb

--- lib/sebes_git_prepush.rb
+++ lib/sebes_git_prepush.rb
@@ -19,6 +19,7 @@ module SebesGitPrepush
       puts 'Installing bin/pre-push...'
       system "[ -f bin/pre-push ] && rm bin/pre-push"
       system "ln -s #{__dir__}/../../lib/scripts/pre-push bin/pre-push"
+      system "chmod +x bin/pre-push"
       
       puts 'Done!'
     end
