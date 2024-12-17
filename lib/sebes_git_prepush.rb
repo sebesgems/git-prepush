5671ff396742e60d576e29f2924b918a3ed06dd9
Artjom Juferov <artjom.juferov@gmail.com>
2024-12-17 08:31:46 +0200

    Version 0.1.1

--- lib/sebes_git_prepush.rb
+++ lib/sebes_git_prepush.rb
@@ -15,17 +15,13 @@ module SebesGitPrepush
       puts 'Installing hooks...'
       system "[ -f #{git_dir}/hooks/pre-push ] && rm #{git_dir}/hooks/pre-push"
       system "ln -s #{__dir__}/../../lib/scripts/pre-push.rb #{git_dir}/hooks/pre-push"
-        
+
       puts 'Installing bin/pre-push...'
-      system "[ -f bin/pre-push ] && rm bin/pre-push"
+      system '[ -f bin/pre-push ] && rm bin/pre-push'
       system "ln -s #{__dir__}/../../lib/scripts/pre-push bin/pre-push"
-      system "chmod +x bin/pre-push"
-      
+      system 'chmod +x bin/pre-push'
+
       puts 'Done!'
     end
   end
 end
-
-# Hooking into Bundler's lifecycle events
-Bundler::Plugin.add_hook('after-install', SebesGitPrepush::Hook.method(:after_install_or_update_spec))
-Bundler::Plugin.add_hook('after-update', SebesGitPrepush::Hook.method(:after_install_or_update_spec))
